bbc577594fd25b151b5f49055d5d0bf9bf328d63
Jonathan Brites <britesjonathan@gmail.com>
2022-08-21 19:00:01 -0300

    commit de novos exercicios

---
+++ aula5/script.js
@@ -0,0 +1,14 @@
+// Falsy
+
+//undefined
+//null
+//0
+//false
+//''
+//NaN - not a numer
+
+//Truthy
+
+let corPersonalizada = 'Vermelho';
+let corPadrao = 'Azul';
+let corPerfil = corPersonalizada || corPadrao;
